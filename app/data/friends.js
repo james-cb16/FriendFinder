1553d48cda0467eb291fe46169ae2b17749296c1
James Bajamundi <jamesbjmnd@gmail.com>
2020-03-19 14:18:51 -0700

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Shocked Girl",
+        photo: "https://media.giphy.com/media/iDITcnfY4x8ve36fpZ/giphy.gif",
+        scores: [
+            "3",
+            "1",
+            "2",
+            "4",
+            "4",
+            "1",
+            "3",
+            "5",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Cool Cat",
+        photo: "https://media.giphy.com/media/3oz8xQQP4ahKiyuxHy/giphy.gif",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "4",
+            "1",
+            "1",
+            "5",
+            "5",
+            "2",
+            "1"
+        ]
+    }
+
+];
+
+module.exports = friends;
